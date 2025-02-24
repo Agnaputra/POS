7871a17e3c068b5219b6623cf460ed3e3c8b2775
nanaputra <agenaputra@gmail.com>
2025-02-24 20:06:10 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\WelcomeController;
 
-Route::get('/greeting', [WelcomeController::class, 'greeting']);
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+// Route Home
+Route::get('/', [HomeController::class, 'index']);
+
+// Route Products dengan prefix 'category'
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Route User dengan parameter ID dan Name
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// Route Penjualan (Sales)
+Route::get('/sales', [SalesController::class, 'index']);
+
+
 
 
