b289534d5842dbd6002bfcf4c34b2cf53304a2d7
nanaputra <agenaputra@gmail.com>
2025-03-17 20:54:09 +0700

    dddd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,59 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\LevelModel;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
+    // Menampilkan halaman daftar user
     public function index()
     {
-        $user = UserModel :: with('level')->get();
-        return view('user', ['data' => $user]);
-        
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'user'; // Set menu yang sedang aktif
+
+        return view('user.index', compact('breadcrumb', 'page', 'activeMenu'));
     }
-    public function tambah()
+
+    // Fetch user data in JSON format for DataTables
+    public function list(Request $request)
     {
-        return view('user_tambah');
-    }
-    public function tambah_simpan(Request $request)
-{
+        $users = UserModel::select('user_id', 'username', 'name', 'level_id')
+            ->with('level');
 
-        UserModel::create([
-        'username' => $request->username,
-        'nama' => $request->nama,
-        'password' => Hash::make('$request->password'),
-        'level_id' => $request->level_id
-
-    ]);
-
-    return redirect('/user');
-    }
-    public function ubah($id)
-    {
-        $user = UserModel :: find($id);
-        return view('user_ubah', ['data' => $user]);
-
-    }
-    public function ubah_simpan($id, Request $request)
-    {
-        $user = UserModel :: find($id);
-
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->password = Hash :: make('$request->password');
-        $user->level_id = $request->level_id;
-
-        $user->save();
-
-        return redirect('/user');
-    }
-    public function hapus($id)
-    {
-        $user = UserModel :: find($id);
-        $user->delete();
-
-        return redirect('/user');
+        return DataTables::of($users)
+            ->addIndexColumn() // Menambahkan kolom index otomatis
+            ->addColumn('action', function ($user) { // Menambahkan kolom aksi
+                $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'" onsubmit="return confirm(\'Are you sure you want to delete this data?\');">
+                            ' . csrf_field() . method_field('DELETE') . '
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>';
+                return $btn;
+            })
+            ->rawColumns(['action']) // Memberitahu bahwa kolom action mengandung HTML
+            ->make(true);
     }
 }
